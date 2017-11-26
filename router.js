c575cb7fc7cf77bad5bc0b54123fb9d7d9f154e8
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2017-11-27 01:24:28 +0200

    Second test working

--- router.js
+++ router.js
@@ -3,17 +3,17 @@
 const router = (request, response) => {
 	
 	console.log(request.url);
-	
+
 	if(request.url==='/'){
 		response.writeHead(200,{'Content-Type':'text/html'});
 		response.write('hello');
 		response.end();
 	}
-	else if (request.url === '/blog' && request.method === 'get'){
+	else if (request.url === '/blog' && request.method === 'GET'){
 		response.writeHead(200, {'Content-Type':'text/html'});
 		let jsonObj = JSON.stringify(['dog', 'cat', 'lion']);
 		response.write(jsonObj);
-		response.end('hello');
+		response.end();
 	}
 	else{
 		response.writeHead(404,{'Content-Type':'text/html'});
--- test.js
+++ test.js
@@ -20,17 +20,24 @@ test('Test home url', (t)=>{
 });
 
 
-test('Test blog url', (t)=>{
-	shot.inject(router, {method:'get',url:'/blog'}, (response)=>{
-		t.equal(response.statusCode, 200 , 'status Code should be 200');
-		t.end();
-	});
-});
-
 test('Test unknown url', (t)=>{
 	shot.inject(router, {method:'get', url:'hhhhhh'}, (response)=>{
 		t.equal(response.statusCode, 404, 'Unknown url should return 404');
 		t.end();
 	});
 });
+
+
+test('Test blog url', (t)=>{
+	shot.inject(router, {method:'get',url:'/blog'}, (response)=>{
+		t.equal(response.statusCode, 200 , 'status Code should be 200');
+		const jsonObj = JSON.parse(response.payload);
+		t.equal(jsonObj.length, 3, "length of array should be 3");
+		t.equal(typeof(jsonObj[0]),'string', 'Element should be string');
+		t.equal(typeof(jsonObj[1]),'string', 'Element should be string');
+		t.equal(typeof(jsonObj[2]),'string', 'Element should be string');
+		t.end();
+	});
+});
+
 
